21fce8584c0aa7590ef2aebf85167bf66c7e72ff
Grich Hiranyaphinant <101271965+EatYoSalmon@users.noreply.github.com>
2022-06-24 02:57:23 +0700

    Improved day 26 nato_alphabet.py with exception handling

--- day26-nato-alphabets-list-comprehension/nato_alphabets.py
+++ day26-nato-alphabets-list-comprehension/nato_alphabets.py
@@ -8,10 +8,17 @@ import pandas as pd
 
 
 PARENT_DIR = str(Path(__file__).parent.resolve())
-df = pd.read_csv(PARENT_DIR + '/nato_phonetic_alphabet.csv')
 
+df = pd.read_csv(PARENT_DIR + '/nato_phonetic_alphabet.csv')
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-word = input("Enter a word: ").upper()
-nato_phonetics = [nato_dict[letter] for letter in word]
-print(nato_phonetics)
+looping = True
+while looping:
+    try:
+        word = input("Enter a word: ").upper()
+        nato_phonetics = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        looping = False
+        print(nato_phonetics)
